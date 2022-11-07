51820d6c7e419f871eb93c07f6a78a10eb886919
Delano Brouwer <Delano@Wearefrank.nl>
2022-11-07 09:40:44 +0100

    Initial Commit
    Uploaded a Spring project with Native compatibility, generated via Spring Initializr.

---
+++ SpringNativeDemo/src/main/java/org/wearefrank/SpringNativeDemo/SpringNativeDemoApplication.java
@@ -0,0 +1,13 @@
+package org.wearefrank.SpringNativeDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNativeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNativeDemoApplication.class, args);
+	}
+
+}
