88ee5ea5d98f0cab17e451d53b8bfb962f1f9c5e
Delano Brouwer <Delano@Wearefrank.nl>
2022-12-13 11:36:32 +0100

    Functional Checkpoint

--- src/main/java/org/ibissource/SpringNativeDemo/SpringNativeDemoApplication.java
+++ src/main/java/org/ibissource/SpringNativeDemo/SpringNativeDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ServletComponentScan 
-@ImportResource("springTestToolTestWebapp.xml")
+@ImportResource("classpath:springTestToolTestWebapp.xml")
 public class SpringNativeDemoApplication {
 
 	public static void main(String[] args) {
