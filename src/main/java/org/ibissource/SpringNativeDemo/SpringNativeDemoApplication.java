56e2fef150b0dcc0aecb2acc088a6d223d06ee4a
Delano Brouwer <Delano@Wearefrank.nl>
2022-12-13 16:38:22 +0100

    Bean not found Error
    Error description:
    No qualifying bean of type 'nl.nn.testtool.echo2.reports.TreePane' available: expected at least 1 bean which qualifies as autowire candidate. Dependency annotations: {@javax.inject.Inject(), @javax.inject.Named("debugTreePane")}
    
    Caused by:
    - Run "mvn spring-boot:run"
    
    Possible solutions:
    -

--- src/main/java/org/ibissource/SpringNativeDemo/SpringNativeDemoApplication.java
+++ src/main/java/org/ibissource/SpringNativeDemo/SpringNativeDemoApplication.java
@@ -3,11 +3,9 @@ package org.ibissource.SpringNativeDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ServletComponentScan 
-@ImportResource("classpath:springTestToolTestWebapp.xml")
 public class SpringNativeDemoApplication {
 
 	public static void main(String[] args) {
